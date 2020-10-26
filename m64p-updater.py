5474c473b90a900ed30ee3bcfdbae2b9f1fc0f35
Logan McNaughton <logan@bacoosta.com>
2020-10-25 18:43:50 -0700

    GUI

--- m64p-updater.py
+++ m64p-updater.py
@@ -1,54 +1,69 @@
 #!/usr/bin/python3
 
 import subprocess
-import time
 import requests
 import tempfile
 import sys
 import os
 import zipfile
 import shutil
+import tkinter as tk
 
 if len(sys.argv) < 2:
     print("no argument!")
     sys.exit(1)
 
-time.sleep(3)
+root = tk.Tk()
+root.geometry("400x200")
+root.title("m64p-updater")
+var = tk.StringVar()
+var.set("Initializing")
+w = tk.Label(root, textvariable=var)
+w.pack(fill="none", expand=True)
 
-resp = requests.get('https://api.github.com/repos/loganmc10/m64p/releases/latest')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-latest = str(resp.json()['id'])
-resp = requests.get('https://api.github.com/repos/loganmc10/m64p/releases/' + latest + '/assets')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for item in resp.json():
-    if sys.platform.startswith('win') and 'win' in item['browser_download_url']:
-        m64p_url = item['browser_download_url']
-    elif sys.platform.startswith('lin') and 'lin' in item['browser_download_url']:
-        m64p_url = item['browser_download_url']
+def update_m64p():
+    var.set("Determining latest release")
+    root.update_idletasks()
+    resp = requests.get('https://api.github.com/repos/loganmc10/m64p/releases/latest')
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    latest = str(resp.json()['id'])
+    resp = requests.get('https://api.github.com/repos/loganmc10/m64p/releases/' + latest + '/assets')
+    if resp.status_code != 200:
+       raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    for item in resp.json():
+        if sys.platform.startswith('win') and 'win' in item['browser_download_url']:
+            m64p_url = item['browser_download_url']
+        elif sys.platform.startswith('lin') and 'lin' in item['browser_download_url']:
+            m64p_url = item['browser_download_url']
 
-resp = requests.get(m64p_url, allow_redirects=True)
-tempdir = tempfile.mkdtemp()
-filename = os.path.join(tempdir, 'm64p.zip')
-open(filename, 'wb').write(resp.content)
+    var.set("Downloading latest release")
+    root.update_idletasks()
+    resp = requests.get(m64p_url, allow_redirects=True)
+    tempdir = tempfile.mkdtemp()
+    filename = os.path.join(tempdir, 'm64p.zip')
+    open(filename, 'wb').write(resp.content)
 
-with zipfile.ZipFile(filename, 'r') as zf:
-    for info in zf.infolist():
-        zf.extract( info.filename, path=tempdir )
-        out_path = os.path.join( tempdir, info.filename )
-        perm = (info.external_attr >> 16)
-        os.chmod( out_path, perm )
+    var.set("Extracting release")
+    root.update_idletasks()
+    with zipfile.ZipFile(filename, 'r') as zf:
+        for info in zf.infolist():
+            zf.extract( info.filename, path=tempdir )
+            out_path = os.path.join( tempdir, info.filename )
+            perm = (info.external_attr >> 16)
+            os.chmod( out_path, perm )
 
-extract_path = os.path.join(tempdir, 'mupen64plus')
-files = os.listdir(extract_path)
-for f in files:
-    shutil.move(os.path.join(extract_path, f), os.path.join(sys.argv[1], f))
+    extract_path = os.path.join(tempdir, 'mupen64plus')
+    files = os.listdir(extract_path)
+    for f in files:
+        shutil.move(os.path.join(extract_path, f), os.path.join(sys.argv[1], f))
 
-shutil.rmtree(tempdir)
+    var.set("Cleaning up")
+    root.update_idletasks()
+    shutil.rmtree(tempdir)
 
-subprocess.Popen([os.path.join(sys.argv[1], 'mupen64plus-gui')])
+    subprocess.Popen([os.path.join(sys.argv[1], 'mupen64plus-gui')])
+    root.destroy()
 
-sys.exit(0)
+root.after(3000, update_m64p)
+root.mainloop()

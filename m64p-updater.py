66e1ece127fed1230e3bb370b0ee96c3fae52132
Logan <loganmc10@users.noreply.github.com>
2020-12-27 23:11:33 -0700

    Update m64p-updater.py

--- m64p-updater.py
+++ m64p-updater.py
@@ -11,12 +11,12 @@ import shutil
 import tkinter as tk
 
 
-def update_m64p():
-    var.set("Determining latest release")
+def update_m64p(root2, var2):
+    var2.set("Determining latest release")
     resp = requests.get(
         'https://api.github.com/repos/loganmc10/m64p/releases/latest')
     if resp.status_code != 200:
-        root.quit()
+        root2.quit()
         return
     for item in resp.json()['assets']:
         if sys.platform.startswith('win') and 'm64p-win64' in item['name']:
@@ -24,7 +24,7 @@ def update_m64p():
         elif sys.platform.startswith('lin') and 'm64p-linux64' in item['name']:
             m64p_url = item['browser_download_url']
 
-    var.set("Downloading latest release")
+    var2.set("Downloading latest release")
     resp = requests.get(m64p_url, allow_redirects=True)
 
     with tempfile.TemporaryDirectory() as tempdir:
@@ -32,7 +32,7 @@ def update_m64p():
         with open(filename, 'wb') as localfile:
             localfile.write(resp.content)
 
-        var.set("Extracting release")
+        var2.set("Extracting release")
         with zipfile.ZipFile(filename, 'r') as zf:
             for info in zf.infolist():
                 zf.extract(info.filename, path=tempdir)
@@ -46,31 +46,33 @@ def update_m64p():
             shutil.move(os.path.join(extract_path, f),
                         os.path.join(sys.argv[1], f))
 
-    var.set("Cleaning up")
-    root.quit()
+    var2.set("Cleaning up")
+    root2.quit()
 
 
-def start_thread():
-    x = threading.Thread(target=update_m64p)
-    x.start()
+def start_thread(x2):
+    x2.start()
 
 
-if len(sys.argv) < 2:
-    print("no argument!")
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("no argument!")
+        sys.exit(1)
 
-my_env = os.environ.copy()
+    my_env = os.environ.copy()
 
-root = tk.Tk()
-root.geometry("400x200")
-root.title("m64p-updater")
-var = tk.StringVar()
-var.set("Initializing")
-w = tk.Label(root, textvariable=var)
-w.pack(fill="none", expand=True)
+    root = tk.Tk()
+    root.geometry("400x200")
+    root.title("m64p-updater")
+    var = tk.StringVar()
+    var.set("Initializing")
+    w = tk.Label(root, textvariable=var)
+    w.pack(fill="none", expand=True)
 
-root.after(3000, start_thread)
-root.mainloop()
+    x = threading.Thread(target=update_m64p, args=(root, var))
+    root.after(3000, start_thread, x)
+    root.mainloop()
+    x.join()
 
-subprocess.Popen([os.path.join(sys.argv[1], 'mupen64plus-gui')],
-                 env=my_env, start_new_session=True, close_fds=True)
+    subprocess.Popen([os.path.join(sys.argv[1], 'mupen64plus-gui')],
+                     env=my_env, start_new_session=True, close_fds=True)
